d9a5b6f244dfaf27c7378405127ac9ed7a03e0e1
ArrobbioUmberto <arrobbio.umberto@gmail.com>
2023-02-01 17:22:21 +0100

    FEAT first part with console log check

--- formapp.js
+++ formapp.js
@@ -13,30 +13,46 @@
 
 
 
-let firstName = document.querySelector('.firstname')
-console.log( 'questo è il nome del passeggero', firstName)
-let lastName = document.querySelector('.lastname')
-console.log('questo è il cognome del passeggero', lastName)
-let numberKm = document.querySelector('.km')
-console.log('questi sono i km che vuole percorrere', numberKm)
+const firstNameElement = document.querySelector(' #firstname')
+const lastNameElement = document.querySelector(' #lastname')
+const numberKmElement = document.querySelector(' #km')
 const ticketPrice = 0.21
+const ageSelector = document.querySelector ('select[name="age"] ' )
+const formBtnElement = document.querySelector('.formbutton input[type="button"')
 
-let totalPrice = numberKm * ticketPrice
 
-let discountYoung = 0.8
 
-let discountOld = 0.6
-if (passengerAge < 18){
-    totalPrice = (numberKm * ticketPrice) * discountYoung
-} else if (passengerAge > 64){
-    totalPrice = (numberKm * ticketPrice) * discountOld
-}
-else {
-    totalPrice = numberKm * ticketPrice
-}
-let decimalTotalKm = parseFloat(totalPrice.toFixed(2))
+formBtnElement.addEventListener('click', function(){
+   console.log( firstNameElement.value)
+    let firstName = firstNameElement.value 
+    console.log( 'questo è il nome del passeggero', firstName)
 
-console.log('questo è il prezzo del biglietto', decimalTotalKm)
+    let lastName = lastNameElement.value 
+    console.log('questo è il cognome del passeggero', lastName)
+
+    let numberKm = numberKmElement.value
+    console.log('questi sono i km che vuole percorrere', numberKm)
+
+    let totalPrice = numberKm * ticketPrice
+
+    let discountYoung = 0.8
+
+    let discountOld = 0.6
+
+
+
+})
+// if (passengerAge < 18){
+//     totalPrice = (numberKm * ticketPrice) * discountYoung
+// } else if (passengerAge > 64){
+//     totalPrice = (numberKm * ticketPrice) * discountOld
+// }
+// else {
+//     totalPrice = numberKm * ticketPrice
+// }
+// let decimalTotalKm = parseFloat(totalPrice.toFixed(2))
+
+// console.log('questo è il prezzo del biglietto', decimalTotalKm)
 
 // SECONDA PARTE ESERCIZIO 
 // cambiare le scelte in minorenne, sconto over65 e nessuno sconto 
